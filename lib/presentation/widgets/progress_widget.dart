000a94f957fa849ec618d4ce195d78dac5dbfa71
Alexander Klapchuk <galaktika2@ukr.net>
2022-12-12 12:24:54 +0200

    Edit gitignore

--- lib/presentation/widgets/progress_widget.dart
+++ lib/presentation/widgets/progress_widget.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
-
 class ProgressWidget extends StatefulWidget {
   const ProgressWidget({Key? key}) : super(key: key);
 
@@ -39,11 +38,7 @@ class _ProgressWidgetState extends State<ProgressWidget> {
 
     // Iterable<CollectionReference> collections = users.doc(user.id)
     return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: users
-            .doc(user.id)
-            .collection('sets')
-            .orderBy('startSet', descending: true)
-            .snapshots(),
+        stream: users.doc(user.id).collection('studies').snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
             return const Center(
@@ -54,82 +49,78 @@ class _ProgressWidgetState extends State<ProgressWidget> {
                 itemCount: snapshot.data!.docs.length,
                 itemBuilder: ((context, index) {
                   DocumentSnapshot setDocument = snapshot.data!.docs[index];
-                  final String dateSet =
-                      _formatDate(setDocument.get('startSet').toDate());
+                  print(setDocument.data());
+                  // final String dateSet =
+                  //     _formatDate(setDocument.get('startSet').toDate());
                   final id = setDocument.id;
-                  return ExpansionTile(
-                      title: Text('Test # $id, Start $dateSet'),
-                      children: [
-                        StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                            stream: users
-                                .doc(user.id)
-                                .collection('sets')
-                                .doc(setDocument.id)
-                                .collection('trials')
-                                .orderBy('startTrial', descending: true)
-                                .snapshots(),
-                            builder: (context, snapshot) {
-                              if (!snapshot.hasData) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              } else {
-                               List<Widget> columnTrails = [];
-                                for (var doc in snapshot.data!.docs) {
-                                  final String dateTrial =
-                                      _formatDate(doc['startTrial'].toDate());
-                                  // print(doc['startTrial']);
-                                  columnTrails.add(
-                                    Container(
-                                      margin: const EdgeInsets.only(
-                                          left: 20,
-                                          top: 20,
-                                          bottom: 20,
-                                          right: 20),
-                                      width: double.infinity,
-                                      height: 100,
-                                      decoration: BoxDecoration(
-                                        color: const Color.fromARGB(
-                                            255, 222, 223, 156),
-                                        borderRadius: const BorderRadius.only(
-                                            topLeft: Radius.circular(10),
-                                            topRight: Radius.circular(10),
-                                            bottomLeft: Radius.circular(10),
-                                            bottomRight: Radius.circular(10)),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.grey.withOpacity(0.5),
-                                            spreadRadius: 5,
-                                            blurRadius: 7,
-                                            offset: const Offset(0,
-                                                3), // changes position of shadow
-                                          ),
-                                        ],
+                  return ExpansionTile(title: Text('Test # $id'), children: [
+                    StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                        stream: users
+                            .doc(user.id)
+                            .collection('studies')
+                            .doc(id)
+                            .collection('modules')
+                            // .orderBy('startTrial', descending: true)
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          } else {
+                            List<Widget> columnTrails = [];
+                            for (var doc in snapshot.data!.docs) {
+                              // final String dateTrial =
+                              //     _formatDate(doc['startTrial'].toDate());
+                              print(doc['startTrial']);
+                              columnTrails.add(
+                                Container(
+                                  margin: const EdgeInsets.only(
+                                      left: 20, top: 20, bottom: 20, right: 20),
+                                  width: double.infinity,
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                    color: const Color.fromARGB(
+                                        255, 222, 223, 156),
+                                    borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                        bottomLeft: Radius.circular(10),
+                                        bottomRight: Radius.circular(10)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 5,
+                                        blurRadius: 7,
+                                        offset: const Offset(
+                                            0, 3), // changes position of shadow
                                       ),
-                                      child: ListTile(
-                                        onTap: () {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) => TrialWidget(
-                                                setId: id,
-                                                trialId: doc.id,
-                                                userId: user.id,
-                                                trialTitle: dateTrial,
-                                              ),
-                                            ),
-                                          );
-                                        },
-                                        title: Text(dateTrial),
-                                      ),
-                                    ),
-                                  );
-                                }
+                                    ],
+                                  ),
+                                  child: ListTile(
+                                    onTap: () {
+                                      // Navigator.push(
+                                      //   context,
+                                      //   MaterialPageRoute(
+                                      //     builder: (context) => TrialWidget(
+                                      //       setId: id,
+                                      //       trialId: doc.id,
+                                      //       userId: user.id,
+                                      //       trialTitle: dateTrial,
+                                      //     ),
+                                      //   ),
+                                      // );
+                                    },
+                                    // title: Text(dateTrial),
+                                  ),
+                                ),
+                              );
+                            }
 
-                                return Column(children: columnTrails);
-                              }
-                            }),
-                      ]);
+                            return Column(children: columnTrails);
+                          }
+                        }),
+                  ]);
                   // var result = snapshot.data.documents[index][];
                 }));
 
