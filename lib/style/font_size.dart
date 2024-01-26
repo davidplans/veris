586431e82760ea27d370b57d346359c7131c25b4
Olexander Klapchuk <galaktika2@ukr.net>
2024-01-26 15:33:22 +0200

    VR-116173440 Add Colors and FontSize

---
+++ lib/style/font_size.dart
@@ -0,0 +1,13 @@
+class FontSize {
+    // FONT FAMILY
+  static const String interFontFamily = 'Inter';
+
+  // FONT SIZE
+  static const double fontSize42 = 40.0;
+  static const double fontSize24 = 24.0;
+  static const double fontSize20 = 20.0;
+  static const double fontSize17 = 17.0;
+  static const double fontSize16 = 16.0;
+  static const double fontSize14 = 14.0;
+  static const double fontSize13 = 13.0;
+}
